71ff03152c0fbb1f075948786a57e3c0fae18c6f
DarrelWanyonyi <darrel.sakwa@gmail.com>
2017-12-06 13:48:27 +0300

    Intitial Commit

---
+++ src/lab/one/Main.java
@@ -0,0 +1,20 @@
+package lab.one;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //In scenario 1, catch the specific exception as indicated by the error
+
+        try {
+            String input = "is";
+            int user_input = Integer.parseInt(input);
+        }catch (NumberFormatException nfe){
+            System.out.println(nfe.getMessage());
+        }
+
+        System.out.println("wrong member type Int");
+
+    }
+
+}
